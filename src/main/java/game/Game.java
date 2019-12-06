0713db5a842ababe0e23f4c840c4bb304151e728
OmnipotentPenguin <adammartin@hotmail.co.uk>
2019-12-06 11:07:42 +0000

    Core Gameplay Loop Created

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -6,21 +6,74 @@ public class Game {
 	
 	public int[] targetCoords = {0,0};
 	public int[] playerLocation = {0,0};
+	public double distance;
+	boolean travelling;
 	
-	public void begin() {
+	public void begin() {	
+		
+		targetCoords[0] = (int) ((Math.random()*10)+5);
+		targetCoords[1] = (int) ((Math.random()*10)+5);
+		
+		System.out.println("Which way do you go?");
+		
 		
 		Scanner s = new Scanner(System.in);
+		travelling = true;
 		
-//		boolean needInput = true;
-//		do {
-//			System.out.println(playRound(s));
-//			System.out.println("Enter 'q' to quit or anything else to continue: ");
-//			play = checkContinue(s.nextLine());
-//		} while (needInput);
+		do {
+			getAction(s.nextLine());		
+			findDistance();
+		} while (travelling);
 		
-		Player p = new Player();	
+		s.close();
 		
 		
+		
+		
+	}
+		
+	private void findDistance() {
+		
+		int xDist = targetCoords[0] - playerLocation[0];
+		int yDist = targetCoords[1] - playerLocation[1];		
+		distance = Math.hypot(xDist, yDist);
+		
+		if (distance == 0) {
+			travelling = false;
+		}
+		
+		System.out.println("Distance left: "+distance);
+		
+	}
+
+	public Actions getAction(String input) {			
+		Actions action = doAction(input);		
+		return action;
+	}
+
+	private Actions doAction(String action) {
+		Player p = new Player();
+		switch (action.toLowerCase()) {
+		case "north":
+			System.out.println("You move North");
+			p.goNorth(playerLocation);			
+			return Actions.NORTH;
+		case "south":
+			System.out.println("You move South");
+			p.goSouth(playerLocation);
+			return Actions.SOUTH; 
+		case "east":
+			System.out.println("You move East");
+			p.goEast(playerLocation);
+			return Actions.EAST;
+		case "west":
+			System.out.println("You move West");
+			p.goWest(playerLocation);
+			return Actions.WEST;
+		default:
+			System.out.println("Sorry, you can't do that here");
+			return null; 
+		}
 	}
 
 }
--- src/main/java/main/App.java
+++ src/main/java/main/App.java
@@ -1,9 +1,13 @@
 package main;
 
+import game.Game;
+
 public class App {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		Game g = new Game();
+		g.begin();
 		
 	}
 
