66269ddbb5cb4c6a71c71586dc5e642684ce2015
OmnipotentPenguin <adammartin@hotmail.co.uk>
2019-12-06 09:50:03 +0000

    Location testing

---
+++ src/test/java/testing/InputTest.java
@@ -0,0 +1,70 @@
+package testing;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class InputTest {
+
+	@Test
+	public void goNorthTest() {
+		Player player = new Player();	
+		
+		int[] pLoc = player.playerLocation;
+		int xLoc = pLoc[0];
+		
+		player.goNorth();
+		
+		int [] pMoved = player.playerLocation;
+		int xMoved = pMoved[0];
+		
+		Assert.assertEquals(xLoc+1, xMoved);
+	}
+	
+	@Test
+	public void goSouthTest() {
+		Player player = new Player();	
+		
+		int[] pLoc = player.playerLocation;
+		int xLoc = pLoc[0];
+		
+		player.goNorth();
+		
+		int [] pMoved = player.playerLocation;
+		int xMoved = pMoved[0];
+		
+		Assert.assertEquals(xLoc-1, xMoved);
+	}
+		
+	@Test
+	public void goEastTest() {
+		Player player = new Player();	
+		
+		int[] pLoc = player.playerLocation;
+		int yLoc = pLoc[0];
+		
+		player.goNorth();
+		
+		int [] pMoved = player.playerLocation;
+		int yMoved = pMoved[0];
+		
+		Assert.assertEquals(yLoc+1, yMoved);
+	}
+}
+	
+	@Test
+	public void goWestTest() {
+		Player player = new Player();	
+		
+		int[] pLoc = player.playerLocation;
+		int yLoc = pLoc[0];
+		
+		player.goNorth();
+		
+		int [] pMoved = player.playerLocation;
+		int yMoved = pMoved[0];
+		
+		Assert.assertEquals(yLoc-1, yMoved);
+	}
+
+}
