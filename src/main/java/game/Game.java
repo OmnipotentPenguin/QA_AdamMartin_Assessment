a4e8c9b39566766d6f8112075074b88ce3beede5
OmnipotentPenguin <adammartin@hotmail.co.uk>
2019-12-06 12:19:11 +0000

    Additional narration

--- src/main/java/game/Game.java
+++ src/main/java/game/Game.java
@@ -1,102 +1,136 @@
 package game;
 
 import java.text.DecimalFormat;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Game {
-	
-	public int[] targetCoords = {0,0};
-	public int[] playerLocation = {0,0};
+
+	public int[] targetCoords = { 0, 0 };
+	public int[] playerLocation = { 0, 0 };
 	public double distance;
 	boolean travelling;
 	int moveCount = 0;
-	
+
 	public void begin() {
-		
+
 		randomLocation();
 		System.out.println(
 				"You wake up, finding yourself in a misty, grey swampland that stretches as far as the eye can see."
-				+ "\nWith no idea of where you are, you have no choice but to WANDER.");
-				
+						+ "\nWith no idea of where you are, you have no choice but to WANDER.");
+
 		Scanner s = new Scanner(System.in);
 		travelling = true;
-		
+
 		while (travelling) {
-			
+
 			Actions action = getAction(s.nextLine());
 			if (action != null) {
 				if (action != Actions.WANDER && moveCount == 0) {
 					System.out.println("You don't know what direction that is in.");
-				}
-				else{
+				} else {
 					reactions(action);
 					narration(moveCount);
 					moveCount++;
 					findDistance();
-				}				
-			}			
-		} //while (travelling);
-		foundLocation();
-		s.close();		
-	}	
-	
-	private void narration(int i) {
-		switch(i) {
-		case 0:
-			System.out.println("You feel something humming lightly on your arm. Looking down, you notice a compass strapped to your wrist."
-					+ "\nThe four cardinal points are on on its face, with a small dial near the bottom reading a distance and the needle is pointing off into the gloom.");
+				}
+			}
 		}
-		
+		foundLocation();
+		s.close();
 	}
 
-	private void randomLocation() {		
-		targetCoords[0] = 1;//(rand.nextInt(5)+3
-		targetCoords[1] = 1;//rand.nextInt(5)+3		
+	private void narration(int i) {
+		switch (i) {
+		case 0:
+			System.out.println(
+					"You feel something humming lightly on your arm. Looking down, you notice a compass strapped to your wrist."
+							+ "\nThe four cardinal points are on on its face, with a small dial near the bottom reading a distance and the needle is pointing off into the gloom.");
+
+		case 5:
+			System.out.println("The gloom seems to be pressing in on you fram all sides...");
+
+		case 10:
+			System.out.println(
+					"You see flickers of movement in the corner of your vision, but when you turn, nothing is there...");
+		}
+
+	}
+
+	private void randomLocation() {
+		Random rand = new Random();
+		targetCoords[0] = rand.nextInt(5) + 3;
+		targetCoords[1] = rand.nextInt(5) + 3;
 	}
 
 	private void findDistance() {
-		
+
 		int xDist = targetCoords[0] - playerLocation[0];
-		int yDist = targetCoords[1] - playerLocation[1];		
+		int yDist = targetCoords[1] - playerLocation[1];
 		distance = Math.hypot(xDist, yDist);
-		
+
 		DecimalFormat df = new DecimalFormat("#.##");
 		String shortDist = df.format(distance);
-		
+
 		if (distance == 0) {
-			travelling = false;			
+			travelling = false;
 		}
-		
-		System.out.println("The compass indicactes there is "+shortDist+"m left.\n\nWhat would you like to do:\n1. Go NORTH\n2. Go EAST\n3. Go SOUTH\n4. Go WEST\n");
-		
+
+		System.out.println("The compass indicactes there is " + shortDist
+				+ "m left.\n\nWhat would you like to do:\n1. Go NORTH\n2. Go EAST\n3. Go SOUTH\n4. Go WEST\n");
+
 	}
 
 	private void foundLocation() {
-		System.out.println("Emerging through the gloom of the swamp, you stumble upon a old house that looks like it's been through better days."
-				+ "\nAs you get closer, you also notice a pair of suspiciously placed glittery red slippers and an attached note."
-				+ "\n\"Put me on and click your heels thrice while thinking of your greatest wish!\"");
-		
+		System.out.println(
+				"Emerging through the gloom of the swamp, you stumble upon a old house that looks like it's been through better days."
+						+ "\nAs you get closer, you also notice a pair of suspiciously placed glittery red slippers and an attached note."
+						+ "\n\"Put me on and click your heels thrice while thinking of your greatest wish!\"");
+
 	}
 
-	public Actions getAction(String input) {			
-		Actions action = doAction(input);		
+	public Actions getAction(String input) {
+		Actions action = doAction(input);
 		return action;
 	}
 
 	private Actions doAction(String action) {
 		Player p = new Player();
 		switch (action.toLowerCase()) {
-		case "north":			
-			p.goNorth(playerLocation);			
+		case "north":
+			p.goNorth(playerLocation);
+			return Actions.NORTH;
+		case "go north":
+			p.goNorth(playerLocation);
+			return Actions.NORTH;
+		case "n":
+			p.goNorth(playerLocation);
 			return Actions.NORTH;
 		case "south":
-			
 			p.goSouth(playerLocation);
-			return Actions.SOUTH; 
-		case "east":			
+			return Actions.SOUTH;
+		case "go south":
+			p.goSouth(playerLocation);
+			return Actions.SOUTH;
+		case "s":
+			p.goSouth(playerLocation);
+			return Actions.SOUTH;
+		case "east":
+			p.goEast(playerLocation);
+			return Actions.EAST;
+		case "go east":
+			p.goEast(playerLocation);
+			return Actions.EAST;
+		case "e":
 			p.goEast(playerLocation);
 			return Actions.EAST;
 		case "west":
+			p.goWest(playerLocation);
+			return Actions.WEST;
+		case "go west":
+			p.goWest(playerLocation);
+			return Actions.WEST;
+		case "w":
 			p.goWest(playerLocation);
 			return Actions.WEST;
 		case "wander":
@@ -104,32 +138,32 @@ public class Game {
 			return Actions.WANDER;
 		default:
 			System.out.println("Sorry, you can't do that here");
-			return null; 
+			return null;
 		}
 	}
-	
+
 	private void reactions(Actions act) {
-		
+
 		switch (act) {
-			case NORTH:
-				System.out.println("\nYou move North\n");
-				break;
-			case SOUTH:
-				System.out.println("\nYou move South\n");
-				break;
-			case EAST:
-				System.out.println("\nYou move East\n");
-				break;
-			case WEST:
-				System.out.println("\nYou move West\n");
-				break;
-			case WANDER:
-				System.out.println("\nYou wander aimlessly\n");
-				break;				
-			default:
-				break;
-				
-		}		
+		case NORTH:
+			System.out.println("\nYou move North\n");
+			break;
+		case SOUTH:
+			System.out.println("\nYou move South\n");
+			break;
+		case EAST:
+			System.out.println("\nYou move East\n");
+			break;
+		case WEST:
+			System.out.println("\nYou move West\n");
+			break;
+		case WANDER:
+			System.out.println("\nYou wander aimlessly\n");
+			break;
+		default:
+			break;
+
+		}
 	}
 
 }
